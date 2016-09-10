2b69447b0da9467cce70ff63a8cc598ad0d96d45
Lappen <lappenatgithub@gmail.com>
2016-09-10 22:35:57 +0200

    Fix storage bug
    when turtle.getItemDetail returned nil

--- api/mc/turtle/waypointplugin/storage.lua
+++ api/mc/turtle/waypointplugin/storage.lua
@@ -33,23 +33,25 @@ function Waypoint.Plugin.Storage:unloadAll(keep_amounts) -- keep_amounts: item_i
 	
 	for i = 1, 16 do
 		local data = turtle.getItemDetail(i)
-		local item_id = Items.get(data.name)
-		
-		if keep_amounts then
-			local keep_amount = keep_amounts:get(item_id)
-			local has_kept_amount = has_kept_amounts:get(item_id)
-			if keep_amount or has_kept_amount then
-				assert(has_kept_amount and keep_amount, "Inconsistent maps!")
-				local slot_keep_amount = math.max(0, math.min(keep_amount - has_kept_amount, turtle.getItemCount(i)))
-				if slot_keep_amount < turtle.getItemCount(i) then
-					Turtle.Abs.drop(self.chest_rotation, turtle.getItemCount(i) - slot_keep_amount, i)
+		if data then
+			local item_id = Items.get(data.name)
+
+			if keep_amounts then
+				local keep_amount = keep_amounts:get(item_id)
+				local has_kept_amount = has_kept_amounts:get(item_id)
+				if keep_amount or has_kept_amount then
+					assert(has_kept_amount and keep_amount, "Inconsistent maps!")
+					local slot_keep_amount = math.max(0, math.min(keep_amount - has_kept_amount, turtle.getItemCount(i)))
+					if slot_keep_amount < turtle.getItemCount(i) then
+						Turtle.Abs.drop(self.chest_rotation, turtle.getItemCount(i) - slot_keep_amount, i)
+					end
+					has_kept_amounts:put(item_id, has_kept_amount + slot_keep_amount)
+				else
+					Turtle.Abs.drop(self.chest_rotation, nil, i)
 				end
-				has_kept_amounts:put(item_id, has_kept_amount + slot_keep_amount)
 			else
 				Turtle.Abs.drop(self.chest_rotation, nil, i)
 			end
-		else
-			Turtle.Abs.drop(self.chest_rotation, nil, i)
 		end
 	end
 
